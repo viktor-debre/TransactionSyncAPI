f3803724a344288c66c1c1794033505ac21d5037
viktor <viktor.debre2002@gmail.com>
2023-01-24 00:45:52 +0200

    Add migration to database for changes and new model

---
+++ TransactionSyncAPI/Migrations/20230123221010_AddTrasactionEntity.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransactionSyncAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrasactionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transactions", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_transactions_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_UserId",
+                table: "transactions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
