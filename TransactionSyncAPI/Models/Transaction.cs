c3efec10ba0806a04d3f0345855e658227cc1907
viktor <viktor.debre2002@gmail.com>
2023-01-23 21:21:45 +0200

    Add Transaction Model

---
+++ TransactionSyncAPI/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace TransactionSyncAPI.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+
+        public string Content { get; set; }
+
+        public string Type { get; set; }
+
+        public string Status { get; set; }
+    }
+}
