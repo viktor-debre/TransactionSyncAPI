541be264f0b6932b30bdbd30079723bee5945bad
viktor <viktor.debre2002@gmail.com>
2023-01-23 19:24:30 +0200

    Add controller to generate authentication token and domain permition of API only for authorized users

---
+++ TransactionSyncAPI/Controllers/TokenController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TransactionSyncAPI.DataAccess;
+using TransactionSyncAPI.Models;
+
+namespace TransactionSyncAPI.Controllers
+{
+    [Route("api/token")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        private readonly TransactionContext _context;
+
+        public TokenController(IConfiguration config, TransactionContext context)
+        {
+            _configuration = config;
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserIndentity userData)
+        {
+            if (userData != null && userData.Email != null && userData.Password != null)
+            {
+                var user = await GetUser(userData.Email, userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("Email", user.Email),
+                        new Claim("Password", user.Password)
+
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(10),
+                        signingCredentials: signIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private async Task<UserIndentity> GetUser(string email, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.Email == email && user.Password == password);
+            return new UserIndentity() { Email= email, Password = password };
+        }
+    }
+}
