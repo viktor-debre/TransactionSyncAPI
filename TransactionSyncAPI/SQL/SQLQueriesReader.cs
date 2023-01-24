83bc9a0d99312180cc8e4bfb1381a9ac95b31f02
viktor <viktor.debre2002@gmail.com>
2023-01-24 14:09:52 +0200

    Add sql queries and provider for them

---
+++ TransactionSyncAPI/SQL/SQLQueriesReader.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace TransactionSyncAPI.SQL
+{
+    public class SQLQueriesReader
+    {
+        public Dictionary<string, string> Queries { get; set;}
+
+        public SQLQueriesReader()
+        {
+            string json = File.ReadAllText("SQL/Queries.json");
+            Queries = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        }
+    }
+}
