c368e9a87d791347550155588cf4fb5e9a6a115d
viktor <viktor.debre2002@gmail.com>
2023-01-24 14:11:30 +0200

    Wrap services adding to separate functions while adding to DI container

---
+++ TransactionSyncAPI/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using TransactionSyncAPI.DataAccess.Interfases;
+using TransactionSyncAPI.DataAccess;
+using TransactionSyncAPI.Services.Intarfaces;
+using TransactionSyncAPI.Services.Realization;
+using TransactionSyncAPI.SQL;
+
+namespace TransactionSyncAPI
+{
+    public static class DependencyInjection
+    {
+        public static void AddServises(IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationJWTService, AuthenticationJWTService>();
+        }
+
+        public static void AddDapperDbConnections(IServiceCollection services)
+        {
+            services.AddScoped<ITransactionDbContext>(provider => provider.GetService<TransactionDbContext>());
+            services.AddScoped<ITransactionWriteDbConnection, TransactionWriteDbConnection>();
+            services.AddScoped<ITransactionReadDbConnection, TransactionReadDbConnection>();
+        }
+
+        public static void AddSqlQueriesProvider(IServiceCollection services)
+        {
+            services.AddSingleton<SQLQueriesReader>();
+        }
+    }
+}
