0425205b0c42faa7220d3edd6a1d80a92e2dc479
viktor <viktor.debre2002@gmail.com>
2023-01-26 14:45:28 +0200

    Add user controller and user service for finding user by name

---
+++ TransactionSyncAPI/Services/Realization/UserService.cs
@@ -0,0 +1,28 @@
+﻿using TransactionSyncAPI.DataAccess.Interfases;
+using TransactionSyncAPI.Models;
+using TransactionSyncAPI.Services.Intarfaces;
+
+namespace TransactionSyncAPI.Services.Realization
+{
+    public class UserService : IUserService
+    {
+        private readonly ITransactionReadDbConnection _readDbConnection;
+
+        public UserService(ITransactionReadDbConnection readDbConnection)
+        {
+            _readDbConnection = readDbConnection;
+        }
+
+        public async Task<IEnumerable<User>> FindUserByName(string firstName, string lastName)
+        {
+            firstName = firstName ?? string.Empty;
+            lastName = lastName ?? string.Empty;
+            var sqlQuery = "SELECT * FROM users WHERE FirstName LIKE @FirstName AND LastName LIKE @LastName";
+            var parameters = new { FirstName = firstName + '%', LastName = lastName + '%' };
+
+            var users = await _readDbConnection.QueryAsync<User>(sqlQuery, parameters);
+
+            return users;
+        }
+    }
+}
